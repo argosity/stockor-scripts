c613395f0a6df62dd973a8c0a28a76a61d22f288
Nathan Stitt <nathan@stitt.org>
2013-09-29 21:18:32 -0500

    Inventory adj to make sure each item has stock

--- lib/demo_data/inventory_adjustments.rb
+++ lib/demo_data/inventory_adjustments.rb
@@ -9,15 +9,21 @@ module DemoData
             @reasons = fetch( :inventory_adjustment_reasons )
             @website = fetch( :websites ).first
             lines = []
+            skus  = []
             DemoData.items.data.each do | item |
-                skuloc = DemoData.skus.skuloc_with_location_and_id( @website.location_id, item.xrefs.first.sku_id )
-                lines.push( { qty: 1, uom_code: 'EA', uom_size: 1, sku_loc_id: skuloc.id } ) unless skuloc.qty > 0
+                xref = item.xrefs.first
+                next if xref.nil? || skus.include?( xref.sku_id )
+                skuloc = DemoData.skus.skuloc_with_location_and_id( @website.location_id, xref.sku_id )
+                next if skuloc.qty.present? && skuloc.qty > 0
+                skus << xref.sku_id
+                raise skuloc unless skuloc.id
+                lines.push( { qty: 1, uom_code: 'EA', uom_size: 1, sku_loc_id: skuloc.id } )
             end
             create({
                     location_id: @website.location_id,
                     description: "Adjustment to bring in stock for items on demo website",
                     reason_id: @reasons.sample.id,
-                    state_event: 'mark_approved',
+                    state_event: 'mark_applied',
                     lines_attributes: lines
                 }) unless lines.empty?
         end
